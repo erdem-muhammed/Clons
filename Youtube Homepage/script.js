09e82ddf8fa5e141323d4514d23fd31066760276
muhammed erdem <erdem.muhammed1@outlook.com>
2021-06-18 22:18:50 +0200

    script.js added

---
+++ Youtube Homepage/script.js
@@ -0,0 +1,19 @@
+var firstmenu = document.getElementById("first.menu");
+var secondmenu = document.getElementById("second.menu");
+
+
+function openmenu
+{
+    if
+    {
+        firstmenu.style.display = "none";
+        secondmenu.style.display = "block":
+        secondmenu.style.width = "10vw";
+    }
+
+    else
+    {
+        secondmenu.style.display = "none";
+        firstmenu.style.display = "block";
+    }
+}
